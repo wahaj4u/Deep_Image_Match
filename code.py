79928ecb4f31282f7cabff071f88306746092916
Wahaj Ul Islam <33065153+wahaj4u@users.noreply.github.com>
2024-12-08 16:24:23 +0500

    Update code.py

--- code.py
+++ code.py
@@ -3,11 +3,7 @@ import requests
 import numpy as np
 import pandas as pd
 import cv2
-from sklearn.metrics.pairwise import cosine_similarity
-from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.models import Model
-import streamlit as st
+
 
 # App title
 st.title("Streamlit Sample App: Image Uploader")
