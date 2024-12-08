1f3ff248c7e8587ab86e80c8b628d4e15f892149
Wahaj Ul Islam <33065153+wahaj4u@users.noreply.github.com>
2024-12-08 15:08:25 +0500

    Update code.py

--- code.py
+++ code.py
@@ -1,11 +1,6 @@
 import streamlit as st
 from PIL import Image
 import numpy as np
-import cv2  # Ensure OpenCV is installed in Colab
-from sklearn.metrics.pairwise import cosine_similarity
-from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.models import Model
 import os
 import requests
 import pandas as pd
