21c0f89282b71653359fa8926265f68f1b8b4102
Wahaj Ul Islam <33065153+wahaj4u@users.noreply.github.com>
2024-12-08 12:28:18 +0500

    Update Code.py

--- Code.py
+++ Code.py
@@ -2,7 +2,7 @@ import os
 import requests
 import numpy as np
 import pandas as pd
-
+from PIL import image
 from sklearn.metrics.pairwise import cosine_similarity
 from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
 from tensorflow.keras.preprocessing import image
