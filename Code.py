6097145927bd5ace92eac03d3e5903d22da7d0b9
Wahaj Ul Islam <33065153+wahaj4u@users.noreply.github.com>
2024-12-08 12:43:14 +0500

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,13 +1,17 @@
-import os
+ import os
 import requests
-from sklearn.metrics import cosine_similarity
+import pandas as pd
+import numpy as np
+import cv2
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.cluster import KMeans
 from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.models import Model
 import streamlit as st
-import cv2
-import pandas as pd
-import numpy as np
+
+print("All libraries are successfully imported!")
+
 
 # Streamlit UI
 st.title("Image Similarity Finder")
